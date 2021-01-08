1ca57251ea312b0960b2c7ff80e663c61a78c98b
kshitij-arora <57871425+kshitij-arora@users.noreply.github.com>
2021-01-08 18:52:45 +0530

    Deployment changes

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: "https://co-shield.herokuapp.com",
       port: 8545,
       network_id: "*" // Match any network id
     }
